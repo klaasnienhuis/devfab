7cd73d4a1ccd1ca87bafe18aeded29cfbaa15b9f
Klaas Nienhuis <mail@klaasnienhuis.nl>
2023-12-29 00:27:20 +0100

    adding real content and replacing regular camera movement with orbital movement

--- docs/components/Scrolling.vue
+++ docs/components/Scrolling.vue
@@ -5,6 +5,7 @@ import { API, Annotation, Material, Node } from "./types";
 const { isDark } = useData();
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { marked } from "marked";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -15,6 +16,9 @@ interface ScrollableAnnotation {
   tx: number;
   ty: number;
   tz: number;
+  lat: number;
+  lon: number;
+  radius: number;
   index: number;
   duration?: number;
 }
@@ -38,12 +42,61 @@ const afterText = ref(null);
 const captionRef = ref(null);
 const api = ref<API | undefined>();
 const annotationList = ref<Annotation[]>([]);
+const scrollModel = ref<ScrollTrigger>();
 
-const setCaption = (annotation: Annotation) => {
-  // if (captionRef.value) {
-  //   captionRef.value.querySelector("h4").innerHTML = annotation.name;
-  //   captionRef.value.querySelector("p").innerHTML = annotation.content.raw;
-  // }
+const state = reactive({
+  annotation: {} as Annotation,
+});
+
+const markdownToHtml = (markdown: string) => {
+  return marked.parse(markdown);
+};
+
+// TODO: get orbital camera data
+const subtractPositions = (position1: number[], position2: number[]) => {
+  const dx = position2[0] - position1[0];
+  const dy = position2[1] - position1[1];
+  const dz = position2[2] - position1[2];
+  return [dx, dy, dz];
+};
+
+const toLatLon = (position: number[], target: number[]) => {
+  const vector = subtractPositions(target, position);
+  const lat = Math.atan2(
+    vector[2],
+    Math.sqrt(vector[0] * vector[0] + vector[1] * vector[1]),
+  );
+  const lon = Math.atan2(vector[1], vector[0]);
+  return [lat, lon];
+};
+
+const getDistance = (position1: number[], position2: number[]) => {
+  const dx = position2[0] - position1[0];
+  const dy = position2[1] - position1[1];
+  const dz = position2[2] - position1[2];
+  return Math.sqrt(dx * dx + dy * dy + dz * dz);
+};
+
+const toPosition = (
+  lat: number,
+  lon: number,
+  distance: number,
+  target: number[],
+) => {
+  const sinLat = Math.sin(lat);
+  const cosLat = Math.cos(lat);
+  const sinLon = Math.sin(lon);
+  const cosLon = Math.cos(lon);
+
+  const x = cosLat * cosLon;
+  const y = cosLat * sinLon;
+  const z = sinLat;
+
+  return [
+    target[0] + x * distance,
+    target[1] + y * distance,
+    target[2] + z * distance,
+  ];
 };
 
 onMounted(() => {
@@ -54,7 +107,13 @@ onMounted(() => {
         api.value = _api;
         _api.addEventListener("viewerready", () => {
           _api.getAnnotationList((_err, annotations: Annotation[]) => {
+            console.log("annotations", annotations);
             annotationList.value = annotations;
+            const latlon = toLatLon(annotations[0].eye, annotations[0].target);
+            const radius = getDistance(
+              annotations[0].eye,
+              annotations[0].target,
+            );
             const scrollableAnnotation: ScrollableAnnotation = {
               ex: annotations[0].eye[0],
               ey: annotations[0].eye[1],
@@ -62,24 +121,24 @@ onMounted(() => {
               tx: annotations[0].target[0],
               ty: annotations[0].target[1],
               tz: annotations[0].target[2],
+              lat: latlon[0],
+              lon: latlon[1],
+              radius,
               index: 0,
             };
             _api.setCameraLookAt(
-              annotations[4].eye,
-              annotations[4].target,
+              annotations[0].eye,
+              annotations[0].target,
               0,
               (_err: Error) => {},
             );
-            //   // setTimeout(() => {
-            //   //   console.log("setUserInteraction");
-            //   //   _api.setUserInteraction(false);
-            //   // }, 1500);
-            //   setCaption(annotations[0]);
+            state.annotation = annotations[0];
             const timeline = gsap.timeline({});
             annotationList.value.forEach(
               (annotation: Annotation, n: number) => {
                 if (n !== 0) {
-                  // _api.hideAnnotation(n);
+                  const latlon = toLatLon(annotation.eye, annotation.target);
+                  const radius = getDistance(annotation.eye, annotation.target);
                   timeline.to(scrollableAnnotation, {
                     duration: 1,
                     ex: annotation.eye[0],
@@ -88,30 +147,32 @@ onMounted(() => {
                     tx: annotation.target[0],
                     ty: annotation.target[1],
                     tz: annotation.target[2],
+                    lat: latlon[0],
+                    lon: latlon[1],
+                    radius,
                     index: n,
+                    ease: "power3.inOut",
                   });
                 }
               },
             );
-            //   let displayed = true;
             //   // Setup the ScrollTrigger with the animation timeline
-            const bib = ScrollTrigger.create({
+            scrollModel.value = ScrollTrigger.create({
               trigger: scrollerRef.value,
               start: "top top",
-              // end: "bottom -1800px",
-              // pin: "#api-iframe",
-              end: "+=3000px",
-              // markers: true,
-              // scrub: true,
+              end: "+=6000px",
+              pin: "#api-iframe",
+              pinSpacer: ".pin-spacer",
+              scrub: true,
               animation: timeline,
-              toggleActions: "play none reverse none",
+              toggleActions: "play none reverse reset",
               // Set the camera look-at position on every update and set the caption text
               onUpdate: (self) => {
                 const currentIndex =
                   self.direction === 1
                     ? Math.floor(scrollableAnnotation.index)
                     : Math.ceil(scrollableAnnotation.index);
-                console.log("currentIndex", currentIndex);
+                state.annotation = annotationList.value[currentIndex];
                 if (currentIndex < annotationList.value.length - 2) {
                   _api.setUserInteraction(false);
                 } else {
@@ -127,31 +188,22 @@ onMounted(() => {
                   scrollableAnnotation.ty,
                   scrollableAnnotation.tz,
                 ];
-                console.log("eye", eye);
-                _api.setCameraLookAt(eye, target, 0, (err: Error) => {
-                  console.log("err", err);
-                });
+                const pos = toPosition(
+                  scrollableAnnotation.lat,
+                  scrollableAnnotation.lon,
+                  scrollableAnnotation.radius,
+                  target,
+                );
+                _api.setCameraLookAt(pos, target, 0, (_err: Error) => {});
               },
             });
+
             ScrollTrigger.create({
               trigger: afterText.value,
-              start: "-=2999px",
+              start: "-=5999px",
               end: "bottom bottom",
               pin: "#text-block",
-              markers: true,
             });
-            //   // setTimeout(() => {
-            //   //   annotationList.value.forEach(
-            //   //     (annotation: Annotation, n: number) => {
-            //   //       ScrollTrigger.create({
-            //   //         trigger: `#annotation_${n}`,
-            //   //         start: "top center",
-            //   //         end: "bottom bottom",
-            //   //         pin: "#content_" + n,
-            //   //       });
-            //   //     },
-            //   //   );
-            //   // }, 1500);
           });
         });
       },
@@ -165,111 +217,104 @@ onMounted(() => {
 <template>
   <div class="flex flex-col place-items-center bg-neutral-900 text-white">
     <a class="back ml-4 mt-4 mr-auto" href="../"> &larr; Back to main page </a>
-    <div class="text-4xl font-extrabold mt-12">Title</div>
-    <div class="w-1/2 mb-16 mt-4">
-      <p>
-        Amet id elit et labore cillum elit sit deserunt do reprehenderit
-        occaecat. Enim ipsum sit do sunt aliqua in est. Exercitation consectetur
-        est anim excepteur dolor. Sit qui deserunt proident excepteur aliquip
-        sunt esse ullamco ex ipsum Lorem minim consectetur. Dolor nostrud amet
-        ad irure qui ea. Fugiat cupidatat anim aliqua duis cupidatat cupidatat
-        nisi.
+    <div class="text-4xl my-12">Statue of A'a</div>
+    <div class="w-1/3 min-w-[500px] mb-12 mt-4">
+      <p class="mb-6 text-white/80">
+        The Asahi Shimbun Displays: Containing the divine a sculpture of the
+        Pacific god A'a
       </p>
-      <p>
-        Amet id elit et labore cillum elit sit deserunt do reprehenderit
-        occaecat. Enim ipsum sit do sunt aliqua in est. Exercitation consectetur
-        est anim excepteur dolor. Sit qui deserunt proident excepteur aliquip
-        sunt esse ullamco ex ipsum Lorem minim consectetur. Dolor nostrud amet
-        ad irure qui ea. Fugiat cupidatat anim aliqua duis cupidatat cupidatat
-        nisi.
+      <p class="mb-6 text-white/80">
+        This example project on devfab.io uses the annotations that are stored
+        in the Sketchfab model and presents them in a scrollable format.
       </p>
-      <p>
-        Amet id elit et labore cillum elit sit deserunt do reprehenderit
-        occaecat. Enim ipsum sit do sunt aliqua in est. Exercitation consectetur
-        est anim excepteur dolor. Sit qui deserunt proident excepteur aliquip
-        sunt esse ullamco ex ipsum Lorem minim consectetur. Dolor nostrud amet
-        ad irure qui ea. Fugiat cupidatat anim aliqua duis cupidatat cupidatat
-        nisi.
+      <p class="mb-6 text-white/80">
+        One of the Pacific's most famous sculptures, this is a figure of a
+        creator god, probably A'a, of the island of Rurutu in the Austral
+        Islands. Its features are formed of thirty small human figures -
+        appropriate for a god of creation. A'a was carved with a hollow head and
+        torso covered by a panel at the back. The cavity may have been designed
+        to hold the skull and bones of a deified ancestor. The British Museum
+        acknowledges contemporary cultural perspectives associated with the
+        objects in its collection. Please note: cultural rights may apply to
+        this object.
       </p>
-      <p>
-        Amet id elit et labore cillum elit sit deserunt do reprehenderit
-        occaecat. Enim ipsum sit do sunt aliqua in est. Exercitation consectetur
-        est anim excepteur dolor. Sit qui deserunt proident excepteur aliquip
-        sunt esse ullamco ex ipsum Lorem minim consectetur. Dolor nostrud amet
-        ad irure qui ea. Fugiat cupidatat anim aliqua duis cupidatat cupidatat
-        nisi.
-      </p>
-      <p>
-        Amet id elit et labore cillum elit sit deserunt do reprehenderit
-        occaecat. Enim ipsum sit do sunt aliqua in est. Exercitation consectetur
-        est anim excepteur dolor. Sit qui deserunt proident excepteur aliquip
-        sunt esse ullamco ex ipsum Lorem minim consectetur. Dolor nostrud amet
-        ad irure qui ea. Fugiat cupidatat anim aliqua duis cupidatat cupidatat
-        nisi.
-      </p>
+    </div>
+    <div class="text-xl mb-8 animate-bounce">
+      Scroll down to explore the statue
     </div>
   </div>
 
-  <!-- <div class="relative"> -->
-  <div class="iframe-wrapper w-screen h-screen bg-pink-500" ref="scrollerRef">
-    <iframe
-      id="api-iframe"
-      allow="autoplay; fullscreen; vr"
-      ref="viewerIframeRef"
-      class="w-full h-full z-10"
-      allowvr
-      allowfullscreen
-      mozallowfullscreen="true"
-      webkitallowfullscreen="true"
-    ></iframe>
+  <div
+    class="iframe-wrapper w-screen h-screen bg-neutral-900"
+    ref="scrollerRef"
+  >
+    <div class="pin-spacer w-full h-full">
+      <div id="api-iframe" class="w-full h-full">
+        <div
+          class="caption bg-neutral-800/80 max-w-[350px] p-6 absolute bottom-0 left-0 m-6 rounded-2xl"
+          ref="captionRef"
+        >
+          <div class="text-2xl text-white font-semibold mb-4">
+            {{ state.annotation.name }}
+          </div>
+          <div
+            v-if="state.annotation.content"
+            class="text-neutral-200 relative"
+            v-html="markdownToHtml(state.annotation.content.raw)"
+          ></div>
+        </div>
+
+        <iframe
+          allow="autoplay; fullscreen; vr"
+          ref="viewerIframeRef"
+          class="w-full h-full z-10"
+          allowvr
+          allowfullscreen
+          mozallowfullscreen="true"
+          webkitallowfullscreen="true"
+        ></iframe>
+      </div>
+    </div>
   </div>
   <div ref="afterText" class="h-screen">
     <div
       id="text-block"
       class="flex flex-col place-items-center bg-neutral-900 text-white"
     >
-      <div class="w-1/2 my-16">
-        <p>
-          Amet id elit et labore cillum elit sit deserunt do reprehenderit
-          occaecat. Enim ipsum sit do sunt aliqua in est. Exercitation
-          consectetur est anim excepteur dolor. Sit qui deserunt proident
-          excepteur aliquip sunt esse ullamco ex ipsum Lorem minim consectetur.
-          Dolor nostrud amet ad irure qui ea. Fugiat cupidatat anim aliqua duis
-          cupidatat cupidatat nisi.
+      <div class="w-1/3 min-w-[500px] mb-16 mt-16">
+        <p class="mb-6 text-white/80">
+          3D model by Thomas Flynn, Canon G7x + Agisoft Photoscan
         </p>
-        <p>
-          Occaecat eu magna id aliqua. Amet cupidatat officia enim eu sunt
-          pariatur tempor consequat culpa ullamco proident. In voluptate ad sit
-          enim ipsum dolore. Aliquip laboris ea non ex incididunt tempor. Ad
-          incididunt esse duis occaecat proident nostrud. Minim non est proident
-          consequat excepteur nulla ad esse exercitation amet id.
+        <div class="text-2xl mb-2">Description</div>
+        <p class="mb-6 text-white/80">
+          God figure known as A'a, carved in anthropomorphic form with 30 small
+          figures over surface of the body and making up the facial features. A
+          lidded cavity in back.
         </p>
-        <p>
-          Dolor consectetur sint excepteur officia quis do mollit commodo
-          deserunt dolor adipisicing. Minim adipisicing aliqua ipsum voluptate
-          elit amet do do ullamco Lorem Lorem consectetur. Amet cillum do
-          laboris nulla.
+        <div class="text-2xl mb-2">Production date</div>
+        <p class="mb-6 text-white/80">
+          17thC (before 1821 (see curatorial comments)) (before 1821 (see
+          curatorial comments))
         </p>
-        <p>
-          Amet id elit et labore cillum elit sit deserunt do reprehenderit
-          occaecat. Enim ipsum sit do sunt aliqua in est. Exercitation
-          consectetur est anim excepteur dolor. Sit qui deserunt proident
-          excepteur aliquip sunt esse ullamco ex ipsum Lorem minim consectetur.
-          Dolor nostrud amet ad irure qui ea. Fugiat cupidatat anim aliqua duis
-          cupidatat cupidatat nisi.
-        </p>
-        <p>
-          Occaecat eu magna id aliqua. Amet cupidatat officia enim eu sunt
-          pariatur tempor consequat culpa ullamco proident. In voluptate ad sit
-          enim ipsum dolore. Aliquip laboris ea non ex incididunt tempor. Ad
-          incididunt esse duis occaecat proident nostrud. Minim non est proident
-          consequat excepteur nulla ad esse exercitation amet id.
-        </p>
-        <p>
-          Dolor consectetur sint excepteur officia quis do mollit commodo
-          deserunt dolor adipisicing. Minim adipisicing aliqua ipsum voluptate
-          elit amet do do ullamco Lorem Lorem consectetur. Amet cillum do
-          laboris nulla.
+        <div class="text-2xl mb-2">Dimensions</div>
+        <div class="mb-6 text-white/80">
+          <p>Height: 116.80 centimetres</p>
+          <p>Width: 36 centimetres</p>
+          <p>Depth: 36 centimetres</p>
+        </div>
+        <div class="text-2xl mb-2">Curator's comments</div>
+        <p class="mb-6 text-white/80">
+          In November 2015, wood samples taken from inside the figure were
+          tested by British Museum scientists and found to be Sandalwood. The
+          wood was too deteriorated to be definitive about the species but it is
+          likely to be Santalum insulare. This information was fed back to the
+          island of Rurutu and the Council of Elders met to discuss it. The
+          Elders chose not to accept the Sandalwood finding, preferring to
+          uphold their own histories which state that A'a was carved from pua
+          wood (Fagraea berteriana). At the same time wood samples from inside
+          the figure's cavity were radiocarbon dated by the Socttish
+          Universities Environmental Research Centre. The results suggest that
+          A'a was carved at some point between 1591 and 1647.
         </p>
       </div>
       <a class="back ml-4 mb-4 mr-auto" href="../">
@@ -277,35 +322,4 @@ onMounted(() => {
       </a>
     </div>
   </div>
-  <!-- <div class="scroller w-screen h-[12000px] top-0" ref="scrollerRef"></div> -->
-  <!-- </div> -->
-
-  <!-- <div class="scroller w-screen h-full top-0" ref="scrollerRef">
-        <div
-          v-for="(annotation, index) in annotationList"
-          :key="index"
-          :id="`annotation_${index}`"
-          class="w-full h-full"
-        >
-          <div
-            class="caption relative bg-neutral-800/80 w-100 max-w-[350px] p-2"
-            :id="`content_${index}`"
-            ref="captionRef"
-          >
-            <h4 class="text-white">{{ annotation.name }}</h4>
-            <p class="text-neutral-200 relative">
-              {{ annotation.content.raw }}
-            </p>
-          </div>
-        </div>
-      </div> -->
-
-  <!-- <v-responsive :aspect-ratio="4 / 3" class="w-100">
-    <iframe
-      style="border: 0"
-      id="api-iframe"
-      ref="viewerIframeRef"
-      class="w-100 h-100 absolute"
-    ></iframe>
-  </v-responsive> -->
 </template>
